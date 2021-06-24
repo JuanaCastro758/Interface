752f25b680bfd81ec67c120cc6d4e731d868daa4
JuanaCastro758 <juanaazucenacastrojimenez@gmail.com>
2021-06-24 17:30:50 -0600

    Se agrega la clase principal

---
+++ src/inicioPrincipal/Interface.java
@@ -0,0 +1,13 @@
+package src.inicioPrincipal;
+import src.vectorVehiculo.VectorVehiculo;
+public class Interface {
+    public static void main(String[] args) {
+        Interface inter=new Interface();
+    }
+    private VectorVehiculo vectorVehiculo=new VectorVehiculo();
+    public Interface(){
+        vectorVehiculo.crearVehi();
+    
+    }
+    
+}
